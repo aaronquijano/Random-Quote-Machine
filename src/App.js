2cb13c18eb7ac04709f4768c27e0bee4c4c5644c
ron23 <ron23.quijano@gmail.com>
2023-04-13 23:15:48 +0800

    Finished

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import React from 'react';
 
-
-
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -10,7 +8,12 @@ class App extends React.Component {
     this.quotes = [
       ['To live is to suffer, to survive is to find some meaning in the suffering.', 'Friedrich Nietzsche'],
       ['The only true wisdom is in knowing you know nothing.', 'Socrates'],
-      ['Know thy self, know thy enemy. A thousand battles, a thousand victories.', 'Sun Tzu']
+      ['Know thy self, know thy enemy. A thousand battles, a thousand victories.', 'Sun Tzu'],
+      ['Do not spoil what you have by desiring what you have not; remember that what you now have was once among the things you only hoped for.', 'Epicurus'],
+      ['It is not wisdom but Authority that makes a law.', 'Thomas Hobbes'],
+      ['Act as if what you do makes a difference. It does.', 'William James'],
+      ['Permanence, perseverance and persistence in spite of all obstacles, discouragements, and impossibilities: It is this, that in all things distinguishes the strong soul from the weak.', 
+        'Thomas Carlyle']
     ];
 
     this.state = {
@@ -20,23 +23,43 @@ class App extends React.Component {
 
     this.handleClick = this.handleClick.bind(this);
   }
+ 
 
   handleClick() {
-    let index = Math.floor((Math.random() * this.quotes.length) + 1);
+    let index = Math.floor((Math.random() * this.quotes.length-1) + 1);
+    let [quote, author] = this.quotes[index];
 
     this.setState({
-      quote: this.quotes[index][0],
-      author: this.quotes[index][1]
+      quote: quote,
+      author: author
     });
   }
 
   render() {
+    let index = 0;
+    let [quote, author] = ['', ''];
+
+    if (this.state.quote === '' || this.state.author === '') {
+      index = Math.floor((Math.random() * this.quotes.length-1) + 1);
+      [quote, author] = this.quotes[index];
+    }
+
     return (
       <blockquote id="quote-box">
-        <p id="text">{this.state.quote}</p>
-        <figcaption id="author"></figcaption>
-        <a id="tweet-quote">Tweet</a>
-        <button id="new-quote" onClick={this.handleClick}>New Quote</button>
+        <p id="text">
+          {(this.state.quote === '') && quote}
+          {(this.state.quote !== '') && this.state.quote}
+        </p>
+        <figcaption id="author">
+          <em>
+            {(this.state.author === '') && author.replace(/^/, "-")}
+            {(this.state.author !== '') && this.state.author.replace(/^/, "-")}
+          </em>
+        </figcaption>
+        <div class="links-container">
+            <a target="_top" href="https://twitter.com/intent/tweet" id="tweet-quote" ><i className="fab fa-brands fa-twitter"></i></a>
+            <button id="new-quote" onClick={this.handleClick}>NEW QUOTE</button>
+        </div>
       </blockquote>
     );
   }
